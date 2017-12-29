65226d449c6f4d0dfd7e9763a948f21c194c3a6d
hivefive <hpdobrica@gmail.com>
2017-12-29 10:44:01 +0100

    add minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 var path = require('path');
 var webpack = require('webpack');
-
+const MinifyPlugin = require("babel-minify-webpack-plugin");
 module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'main.bundle.js'
     },
+    plugins: [
+        new MinifyPlugin({}, {
+            test: /\.js$/,
+            sourceMap: ""
+        })
+    ],
     module: {
         loaders: [
             {
